918e2572c92262cc66c72d119d77f84b62f8faa8
yjyj0234 <yjyj0345@naver.com>
2025-08-04 11:38:36 +0900

    git commit -m 깃세팅 git commit -m

---
+++ src/main/java/boot/data/jobhub/JobhubApplication.java
@@ -0,0 +1,13 @@
+package boot.data.jobhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boot/data/jobhub/JobhubApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.data.jobhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
