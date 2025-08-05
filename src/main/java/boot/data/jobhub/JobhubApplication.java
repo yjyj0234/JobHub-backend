472f581be17c5b31ecc409f3e3e2179ec3654d1a
yjyj0234 <yjyj0345@naver.com>
2025-08-05 16:49:31 +0900

    스프링구조수정

--- src/main/java/boot/data/jobhub/JobhubApplication.java
+++ src/main/java/boot/data/jobhub/JobhubApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("boot.data.*")
+@EnableJpaRepositories("boot.data.*") 
 @EntityScan("boot.data.*")
 @ComponentScan("boot.data.*")
 public class JobhubApplication {
