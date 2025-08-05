b216db8beb04d8a3374781998e5de6a014e8a85e
yjyj0234 <yjyj0345@naver.com>
2025-08-05 19:33:42 +0900

    0805플젝DTO

---
+++ src/main/java/boot/data/dto/CompanyDetails.java
@@ -0,0 +1,47 @@
+package boot.data.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "company_details")
+public class CompanyDetails {
+	
+	@Id
+	private Long company_id;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@MapsId
+	@JoinColumn(name = "company_id")
+	private Companies company;
+	
+    @Column(name = "website_url", length = 255)
+	private String website_url;
+	
+    @Lob
+	private String logo_url;
+	
+    @Lob
+	private String description;
+	
+    @Lob
+	private String mission;
+    @Lob
+	private String culture;
+	
+    //JSON 필드는 String 으로 처리
+    @Lob
+	private String benefits;
+	
+	
+
+}
---
+++ src/main/java/boot/data/dto/Skill_Categories.java
@@ -0,0 +1,21 @@
+package boot.data.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "skill_categories")
+public class Skill_Categories {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+}
