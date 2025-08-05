a6ca21fce8b865cfd1a9a207049e1dd9c7175fa6
ppiphd <pushprap00@naver.com>
2025-08-05 16:45:39 +0900

    security set

--- src/main/java/boot/data/dto/UserDto.java
+++ src/main/java/boot/data/dto/UserDto.java
@@ -1,8 +1,6 @@
 package boot.data.dto;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
