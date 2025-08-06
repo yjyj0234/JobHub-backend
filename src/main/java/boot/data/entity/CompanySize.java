bad9c1013ea9efb33ad8774c9fc49ca95fbe27e6
현승윤 <gustmddbns@naver.com>
2025-08-06 12:08:06 +0900

    hyeon수정

--- src/main/java/boot/data/entity/CompanySize.java
+++ src/main/java/boot/data/entity/CompanySize.java
@@ -1,5 +1,30 @@
 package boot.data.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "company_sizes")
 public class CompanySize {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //autoIncrement 인경우
+    private Integer id;
+
+    @Column(nullable = false,length = 100)
+    private String label; //규모 이름 (스타트업, 중소기업, 대기업 등)
+
+    @Column(name = "min_employees")
+    private Integer minEmployees;  //최소 직원수
+
+    @Column(name = "max_employees")
+    private Integer maxEmployees; //최대 직원수
+
 }
