182a6639f47c1d65238d610d06fe306ce1453fa4
LWH0419 <leewh0519@naver.com>
2025-08-06 17:56:25 +0900

    0806[이우형] 백 수정

--- src/main/java/boot/data/controller/CommunityController.java
+++ src/main/java/boot/data/controller/CommunityController.java
@@ -3,14 +3,17 @@ package boot.data.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import boot.data.entity.CommunityPosts;
+import boot.data.entity.Users;
+import boot.data.repository.UserRepository;
 import boot.data.service.CommunityService;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/community")
 public class CommunityController {
     
@@ -18,8 +21,10 @@ public class CommunityController {
     private CommunityService service;
 
     @PostMapping("/addpost")
-    public void insertPost(CommunityPosts posts)
+    public void insertPost(@RequestBody CommunityPosts posts)
     {
+        
+        
         service.insertPost(posts);
     }
 }
