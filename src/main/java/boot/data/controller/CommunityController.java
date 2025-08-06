f905b1dd242cb7dcb4c4bb79407069f5ca0ecaf2
LWH0419 <leewh0519@naver.com>
2025-08-06 16:23:44 +0900

    0806

--- src/main/java/boot/data/controller/CommunityController.java
+++ src/main/java/boot/data/controller/CommunityController.java
@@ -1,12 +1,25 @@
 package boot.data.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import boot.data.entity.CommunityPosts;
+import boot.data.service.CommunityService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/community")
 public class CommunityController {
     
+    @Autowired
+    private CommunityService service;
+
+    @PostMapping("/addpost")
+    public void insertPost(CommunityPosts posts)
+    {
+        service.insertPost(posts);
+    }
 }
