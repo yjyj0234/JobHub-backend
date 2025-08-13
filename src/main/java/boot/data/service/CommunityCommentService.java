328f956e8c242f37d9dcd61e47ef5c57a26b159c
LWH0419 <leewh0519@naver.com>
2025-08-13 10:13:11 +0900

    0812[이우형]

--- src/main/java/boot/data/service/CommunityCommentService.java
+++ src/main/java/boot/data/service/CommunityCommentService.java
@@ -2,7 +2,10 @@ package boot.data.service;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CommunityCommentService {
     
 }
