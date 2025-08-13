9da56feddd25ac51eab70be6a6734afe54dc5a50
현승윤 <gustmddbns@naver.com>
2025-08-13 14:39:19 +0900

    [현승윤]커밋

--- src/main/java/boot/data/controller/JobPostingController.java
+++ src/main/java/boot/data/controller/JobPostingController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/api/postings")
+@RequestMapping("api/postings")
 @RequiredArgsConstructor
 public class JobPostingController {
 
