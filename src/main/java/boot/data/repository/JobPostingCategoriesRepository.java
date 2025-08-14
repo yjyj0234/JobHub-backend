4835d63dda271b418870699b9b4950f9365a7863
yjyj0234 <yjyj0345@naver.com>
2025-08-14 19:21:49 +0900

    dta

--- src/main/java/boot/data/repository/JobPostingCategoriesRepository.java
+++ src/main/java/boot/data/repository/JobPostingCategoriesRepository.java
@@ -1,6 +1,21 @@
 package boot.data.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import boot.data.entity.JobPostingCategories;
 
-public interface JobPostingCategoriesRepository extends JpaRepository<JobPostingCategories, Long> {}
+public interface JobPostingCategoriesRepository extends JpaRepository<JobPostingCategories, Long> {
+
+@Query("""
+        select c
+        from JobPostingCategories c
+        join fetch c.jobCategory cat
+        where c.jobPosting.id = :jobId
+        order by c.isPrimary desc, c.id asc
+    """)
+    List<JobPostingCategories> findByJobIdWithCategory(Long jobId);
+
+}
